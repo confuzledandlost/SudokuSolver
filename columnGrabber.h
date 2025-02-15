f8ff342f5598ef131ecfacdd47fa606c73271c52
brandonuscg <brandon.clark.robinson@gmail.com>
2025-02-14 23:38:08 -0800

    Refactored columnGrabber.h and gridGrabber.h to be in-line with rowGrabber.h void pointer logic. Fixed checker.h as well, multiple silly off-by-one errors.

--- columnGrabber.h
+++ columnGrabber.h
@@ -8,12 +8,12 @@
 
 
 // Function that grabs numbers from an array in a "column"
-// Takes in a Mission struct pointer that has an int ID and an array of ints, returns a boolean
-bool columnGrabber(struct Mission* a1) {
+// Takes in a void pointer that has an int ID and an array of ints, and array of chars
+void* columnGrabber(void* ptr) {
 
-    // Setup an array of ints and grab the column number
-    int colVals[9];
-    int colIndex = a1->id;
+    struct Mission* a1 = (struct Mission*) ptr;     // Cast void ptr into mission ptr
+    int colVals[9];       // Setup an array of ints
+    int colIndex = a1->id;   // Grab the column number
 
     // Go through the Mission->array
     for (int i = 0; i < 9; i++) {
@@ -23,12 +23,19 @@ bool columnGrabber(struct Mission* a1) {
     }
 
     // Pass the column values to the checker function
-    if(checker(colVals)) {
-        return true;    // If it checks out, return true
-    } else {
-        printf("Column %d doesn't have the required values \n", a1->id);
-        return false;    // If not, return false
+    if(!checker(colVals)) {
+
+        // If it doesn't check out not, write a message into the a1->msg array
+        size_t bufSize = snprintf(NULL, 0, "Column %d doesn't have the required values.\n", a1->id) + 1;
+        a1->msg = malloc(bufSize);
+        sprintf(a1->msg, "Column %d doesn't have the required values.\n", a1->id);
     }
 }
 
 #endif //COLUMNGRABBER_H
+
+// Example output should be formatted as such:
+// Row 4 doesn't have the required values.
+// Column 3 doesn't have the required values.
+// The middle left subgrid doesn't have the required values.
+// The input is not a valid Sudoku.
